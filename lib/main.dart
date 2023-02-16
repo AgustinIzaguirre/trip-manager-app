215575fef8f1b00e324164350cc4b8fd3f8b89e9
AgustinIzaguirre <Agustinizag1@gmail.com>
2023-02-16 19:06:42 -0300

    Feature/initial scaffolding (#1)
    * Autogenerated app
    
    * Refactor file hierarchy

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './pages/home_page.dart';
+
+void main() {
+  runApp(const TripManager());
+}
+
+class TripManager extends StatelessWidget {
+  const TripManager({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
