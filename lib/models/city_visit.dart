26a15015f72274be4679f2ffb1eef8d19eec1abb
Agustin Izaguirre <agustinizag1@gmail.com>
2023-02-16 19:26:29 -0300

    Add CityVisit and Trip model

--- lib/models/city_visit.dart
+++ lib/models/city_visit.dart
@@ -1,10 +1,28 @@
-class City {
+class CityVisit {
   final String arrivalDate;
   final String departureDate;
   final String name;
   final String country;
   final List<String> todos;
+  int daysOfStay;
 
-  _City(
-  final String arrivalDate, final String departureDate, final String name, final String country, List<String> todos,)
+  CityVisit._(
+    this.arrivalDate,
+    this.departureDate,
+    this.name,
+    this.country,
+    this.todos,
+  ) {
+    daysOfStay = 1; //TODO calculate from arrival and departure date
+  }
+
+  static CityVisit of(
+    final String arrivalDate,
+    final String departureDate,
+    final String name,
+    final String country,
+    List<String> todos,
+  ) {
+    return CityVisit._(arrivalDate, departureDate, name, country, todos);
+  }
 }
