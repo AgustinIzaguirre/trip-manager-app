23b0c0ad70388d5fa64e54aa9419d9208336f7ed
Agustin Izaguirre <agustinizag1@gmail.com>
2023-02-16 19:26:20 -0300

    Add CityVisit and Trip model

---
+++ lib/models/city_visit.dart
@@ -0,0 +1,10 @@
+class City {
+  final String arrivalDate;
+  final String departureDate;
+  final String name;
+  final String country;
+  final List<String> todos;
+
+  _City(
+  final String arrivalDate, final String departureDate, final String name, final String country, List<String> todos,)
+}
